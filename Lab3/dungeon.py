70e59690824efdc1bb00b2e793d2bbd75af7d3cd
jasii <josephsepe@gmail.com>
2021-04-18 19:05:11 -0700

    finished lab 3

--- Lab3/dungeon.py
+++ Lab3/dungeon.py
@@ -1,6 +1,8 @@
-from hashlib import new
 from constant import *
 import random
+
+def display_instructions():
+    print("Welcome to the dungeon. Collect gold and try to make it out alive.")
 
 def create_dungeon(dungeon, num_traps, num_treasures, trap_locations, treasure_locations, exit_location):
     spaces_taken = []
@@ -8,38 +10,58 @@ def create_dungeon(dungeon, num_traps, num_treasures, trap_locations, treasure_l
     # randomly place traps
     i = 0
     while i < num_traps:
+        # generate random row, col
         random_row = random.randint(0,MAX_SIZE-1)
         random_col = random.randint(0,MAX_SIZE-1)
+        # if space isn't taken
         if [random_row, random_col] not in spaces_taken:
-            dungeon[random_row][random_col] = 'T'
+            # draw trap
+            dungeon[random_row][random_col] = TRAP
+            # add location to spaces taken
             spaces_taken += [[random_row, random_col]]
+            # add trap location to trap location list
             trap_locations += [[random_row, random_col]]
             i += 1
     # randomly place treasures
     i = 0
     while i < num_treasures:
+        # generate random row, col
         random_row = random.randint(0,MAX_SIZE-1)
         random_col = random.randint(0,MAX_SIZE-1)
+        # if space isn't taken
         if [random_row, random_col] not in spaces_taken:
-            dungeon[random_row][random_col] = 'G'
+            # draw gold
+            dungeon[random_row][random_col] = GOLD
+            # add location to spaces taken
             spaces_taken += [[random_row, random_col]]
+            # add treasure location to treasure location list
             treasure_locations += [[random_row, random_col]]
             i += 1         
     #randomly place player
     while player_location == []:
+        # generate random row, col
         random_player_row = random.randint(0,MAX_SIZE-1)
         random_player_col = random.randint(0,MAX_SIZE-1)
+        # if space isn't taken
         if [random_player_row, random_player_col] not in spaces_taken:
-            dungeon[random_player_row][random_player_col] = 'P'
+            # draw player
+            dungeon[random_player_row][random_player_col] = PLAYER
+            # add location to spaces taken
             spaces_taken += [[random_player_row, random_player_col]]
+            # set player location
             player_location = random_player_row, random_player_col
     #randomly place exit
     while exit_location == []:
+        # generate random row, col
         random_exit_row = random.randint(0,MAX_SIZE-1)
         random_exit_col = random.randint(0,MAX_SIZE-1)
+        # if space isn't taken
         if [random_exit_row, random_exit_col] not in spaces_taken:
-            dungeon[random_exit_row][random_exit_col] = 'E'
+            # draw exit
+            dungeon[random_exit_row][random_exit_col] = EXIT
+            # add location to spaces taken
             spaces_taken += [[random_exit_row, random_exit_col]]
+            # set exit location
             exit_location += [random_exit_row, random_exit_col]
     
     #return player starting location as a tuple
@@ -54,14 +76,16 @@ def display_dungeon(dungeon):
 
 
 def get_move(player_location):
-
     legal_move = False
+    # while not a legal move, get a legal move
     while legal_move == False:
-
         #get move from from user and validate
         print("Move your character with W, A, S, or D")
         player_move = get_and_check_valid_move_input()
+        # previous location
+        current_location = player_location
 
+        # move the player up down left or right
         if player_move == 'w':
             player_location = player_location[0]-1, player_location[1]
         elif player_move == 'a':
@@ -71,8 +95,11 @@ def get_move(player_location):
         elif player_move == 'd':
             player_location = player_location[0], player_location[1]+1
         
+        # not inside the dungeon
         if (player_location[0] not in range(0, MAX_ROW)) or (player_location[1] not in range(0, MAX_COL)):
             print("Out of bounds. Not a legal move")
+            # set player location back to previous location
+            player_location = current_location
             legal_move = False
         else: #valid location inside dungeon
             legal_move = True
@@ -81,6 +108,7 @@ def get_move(player_location):
 
 # Passes in the move and then returns w,a,s,d
 def get_and_check_valid_move_input():
+    # valid character list
     valid = ['w', 'a', 's', 'd']
     answer = None
     while answer == None:
@@ -93,8 +121,10 @@ def get_and_check_valid_move_input():
     return answer
 
 def check_move(player_next_location, type_of_object_locations):
+    # assign and set player_move to player_next_location
     player_move = list(player_next_location)
     move = None
+    # if player_move is in the list of object locations or equals object location return true
     if player_move in list(type_of_object_locations) or player_move == list(type_of_object_locations):
         move = True
     else:
@@ -104,7 +134,9 @@ def check_move(player_next_location, type_of_object_locations):
 def update_dungeon(dungeon, player_current_location, player_next_location):
     current_row, current_col = player_current_location
     next_row, next_col = player_next_location
+    # set player's new location to P
     dungeon[next_row][next_col] = PLAYER
+    # set player's last location to a .
     dungeon[current_row][current_col] = SPACE
 
 
--- Lab3/main.py
+++ Lab3/main.py
@@ -4,10 +4,10 @@ from constant import *
 from dungeon import *
 
 def main():
-    print("Start game")
     playing = True
     
     while playing:
+        # Initialize game
         dungeon = []
         for index in range(MAX_SIZE):
             dungeon.append([SPACE] * MAX_COL)
@@ -19,35 +19,44 @@ def main():
         treasure_locations = []
         exit_location = []
 
+        # check if board is too small for number of traps + treasure + player and exit
         if MAX_SIZE**2 < (NUM_TRAPS + NUM_TREASURES + 2):
             print("Game board is too small")
             exit()
         else:
+            # draw dungeon, traps, treasures, exit, and return player location
             player_current_location = create_dungeon(dungeon, NUM_TRAPS, NUM_TREASURES, trap_locations, treasure_locations, exit_location)
             display_dungeon(dungeon)
-            #display_instructions()
+            display_instructions()
+
+            # haven't won or lost
             while not win and not lose:
                 player_next_location = get_move(player_current_location)
             
                 # check if stepped on exit
                 win = check_move(player_next_location, exit_location)
-                #print(check_move(player_next_location, exit_location))
                 
-                if not win:
-                    # check if stepped on a trapd
-                    if check_move(player_next_location, trap_locations):
-                        lose = True
+                # check if stepped on a trap
+                lose = check_move(player_next_location, trap_locations)
                 
-                if not win and not lose:
-                    # check if stepped on treasure
-                    if check_move(player_next_location, treasure_locations):
-                        #TODO Remove treasure location
-                        gold += 1
-                        print("+1 Gold")
-                    
-                    update_dungeon(dungeon, player_current_location, player_next_location)
-                    player_current_location = player_next_location
-                    display_dungeon(dungeon)
+                # check if stepped on treasure
+                if check_move(player_next_location, treasure_locations):
+                    # Remove treasure location
+                    treasure_locations.remove(list(player_next_location))
+                    # Add 1 gold
+                    gold += 1
+                    print("+1 Gold")
+
+                    # Got all the gold
+                    if len(treasure_locations) == 0:
+                        print("You collected all the gold. Make it to the exit!")
+                
+                # update dungeon with player's next location
+                update_dungeon(dungeon, player_current_location, player_next_location)
+                # reset the player's current location
+                player_current_location = player_next_location
+                # draw dungeon
+                display_dungeon(dungeon)
                 
             # Report if won or lost
             if lose:
